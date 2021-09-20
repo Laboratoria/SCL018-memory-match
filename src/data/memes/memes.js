20c2532ddbe13fc0f62e515e25b65208c1e1e662
visnupriya <visnupriya.devi108@gmail.com>
2021-09-20 13:04:54 -0400

    carpeta de fotos y memes.js

---
+++ src/data/memes/memes.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'memesgatos',
+    items: [
+      { id: 'programador', image: 'gatos memes\alternativa de programador.jpg', bgColor: '#339933' },
+      { id: 'dieta', image: 'gatos memes\como va la dieta .jpg', bgColor: '#339933' },
+      { id: 'pinterest', image: 'gatos memes\el de pinterest.jpg', bgColor: '#339933' },
+      { id: 'anvorguesa', image: 'gatos memes\fue demasiada anvorguesa.jpg', bgColor: '#F05032' },
+      { id: 'cuarentena', image: 'gatos memes\la cuarentena me a hecho daño.jpg', bgColor: '#F05032' },
+      { id: 'siento', image: 'gatos memes\lo que siento por ti .jpg', bgColor: '#F05032' },
+     
+    ],
+  };
