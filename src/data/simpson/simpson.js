dd1948ac01ae7b6800d79880c87f24aa4a03c242
visnupriya <visnupriya.devi108@gmail.com>
2021-09-23 16:07:31 -0400

    carpeta linkeada de simpson y css con tarjeta 1

---
+++ src/data/simpson/simpson.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'simpson',
+    items: [
+      { id: 'marge', image: 'https://i.imgur.com/96NVlic.png', bgColor: '#339933' },
+      { id: 'maggie', image: 'https://i.imgur.com/qWhUEFw.png', bgColor: '#339933' },
+      { id: 'lisa', image: 'https://i.imgur.com/ursOuml.png', bgColor: '#339933' },
+      { id: 'homero', image: 'https://i.imgur.com/dDZgcys.png', bgColor: '#F05032' },
+      { id: 'abraham', image: 'https://i.imgur.com/YIed574.png', bgColor: '#F05032' },
+      { id: 'bart', image: 'https://i.imgur.com/shc45uL.png', bgColor: '#F05032' },
+     
+    ],
+  };
