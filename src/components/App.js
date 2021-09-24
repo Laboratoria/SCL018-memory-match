ecba0715ad3ac47f29e86ab8c59b34e085eff996
visnupriya <visnupriya.devi108@gmail.com>
2021-09-24 15:33:47 -0400

    carpeta de imagenes y cambios en css

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
- import pokemon from '../data/pokemon/pokemon.js';
-console.log(pokemon);
+ import simpson from '../data/simpson/simpson.js';
+//console.log(simpson);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -18,38 +18,55 @@ const App = () => {
 
   //1) Crear una variable que contenga las imágenes a utilizar en las tarjetas, llamandolas desde el objeto en main.js.
 
-  let imagenes = pokemon.items; 
-  console.log(imagenes);
+  let imagenes = simpson.items; /*estamos al objeto simpson pero solo al array items*/ 
+  //console.log(imagenes);
 
   //2) Duplicar estas imágenes con la método concat() para que queden en pares.
 
-  //let totalImagenes = imagenesconcat(imagenes);
+  let totalImagenes = imagenes.concat(imagenes);
+  console.log(totalImagenes);
 
   //3) Crear una función que reparta las tarjetas en un html dinámico.
 
-  let reparteTarjetas =()=> {
+  function reparteTarjetas() {
 
-    /*let mesa = document.getElementById("tablero").innerHTML = " ";
-    imagenes.forEach((elemento)=>{
-        let tarjeta = document.createElement("div");
-        tarjeta.innerHTML=
-        <div class="area-tarjeta">
-        <div class="tarjeta">
-        <div class="cara-trasera"> + elemento + </div>
-        <div class="cara-superior">
-        </div>
-        </div>
-        </div>
-        mesa.appendChild(tarjeta);
-    });
-}*/
+    let mesa  = document.getElementById("tablero");
 
-  const el = document.createElement('div');
+    mesa.innerHTML = " ";
+
+    totalImagenes.forEach(myFunction);
+
+    function myFunction(imagen) {
+
+      let tarjeta = document.createElement("div");
+
+      tarjeta.innerHTML =
+
+     `<div class="area-tarjeta">
+      <div class="tarjeta">
+      <div class="cara-trasera"> + ${imagen} + </div>
+      <div class="cara-superior">
+      </div>
+      </div>
+      </div>`
+
+      mesa.appendChild(tarjeta);
+
+    }
+
+  }
+  reparteTarjetas();
+}
+  
+
+
+
+  /*const el = document.createElement('div');
 
   el.className = 'App';
   el.textContent = '';
 
   return el;
-};
+};*/
 
 export default App;
