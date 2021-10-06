188eb041a7f134ac5f655898b283b73591a29e04
Mire~ <65975589+Mire-Scarlet@users.noreply.github.com>
2021-10-06 11:04:31 -0300

    comentarios en main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import App from './components/App.js';
 import data from './data/Ghibli/Ghibli.js';
 
-document.getElementById('root').appendChild(App());
+//Qué parte del código vamos a escribir aquí?
+//Parece que no va nada
+
+document.getElementById('root').appendChild(App());// Desde el HTML se toma root para crearle un hijo desde App.js
