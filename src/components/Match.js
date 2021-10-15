763abb87b75ed25b795a6069b174d94924ff760c
Ysandys Perez <ysandysp@gmail.com>
2021-10-15 10:39:23 -0300

    opciones para el match

--- src/components/Match.js
+++ src/components/Match.js
@@ -5,15 +5,11 @@
 -Comparar similitud de carta 1 y carta 2
 -Si son similares en algún atributo, es match (true) y quedan fijas
 -De lo contrario, son false y se deben volver a voltear*/
-
-
-
 const matchCards = (array) => {
   let matchPos = [];
     for(let i = 0; i < array.length; i++){
     if (array.length == 2 && array[0] == array[1]) {
-      matchPos.push(array[i]);
-      console.log(matchPos);
+      matchPos.push(array);
       alert('hicieron match');
       /*score += 100;
       numberScore.innerHTML = score;
@@ -23,10 +19,11 @@ const matchCards = (array) => {
       /*array[0].matched = true;
       array[1].matched = true;*/
       array.length = 0;
-    } else if(array.length == 2 && array[0] !== array[1]){
-      /*array.style.transform = 'rotateY(0deg)'*/
+    } else /*if(array.length == 2 && array[0] !== array[1])*/{
+      array.classList.remove('flipCard');
       alert('no hicieron match');
+      //array.style.transform= "rotateY(180deg)";
       array.length = 0;
-    }}}
+    }}};
     
 export default matchCards;
